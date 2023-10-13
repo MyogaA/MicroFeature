27d9e131397117db491b848815573a8eb857be1c
M Yoga Atmajaya <myogaatmajaya@gmail.com>
2023-10-13 17:46:01 +0700

    MicroFeature

--- src/home.tsx
+++ src/home.tsx
@@ -6,19 +6,19 @@ const Home = () => {
     <Center h="100vh" bgImg="https://wallpapersmug.com/download/1440x900/ba787c/black-dark-cubes-abstract.jpg">
       <Box>
         <Text fontSize="xl"color="white" textAlign="center">MicroFeature</Text>
-        <Button as={Link} to="/countdown" variant="outline" colorScheme="blue" _hover={{
+        <Button as={Link} to="/countdown" variant="outline" colorScheme="red" _hover={{
               backgroundColor: "red.500",
               color: "black",
             }} m={2} w="100%">
           CountDown
         </Button>
-        <Button as={Link} to="/tictactoe" variant="outline" colorScheme="blue" _hover={{
+        <Button as={Link} to="/tictactoe" variant="outline" colorScheme="yellow" _hover={{
               backgroundColor: "yellow.500",
               color: "black",
             }} m={2} w="100%">
           Tictactoe
         </Button>
-        <Button as={Link} to="/wordSramble" variant="outline" colorScheme="blue" _hover={{
+        <Button as={Link} to="/wordSramble" variant="outline" colorScheme="green" _hover={{
               backgroundColor: "green.500",
               color: "black",
             }} m={2} w="100%">
